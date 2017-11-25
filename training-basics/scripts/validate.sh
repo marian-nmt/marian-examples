01db041b4527b9f8c20cce9dbec4469e34faf670
Marcin Junczys-Dowmunt <junczys@amu.edu.pl>
2017-11-25 15:24:17 +0000

    fix wrong detok

--- training-basics/scripts/validate.sh
+++ training-basics/scripts/validate.sh
@@ -3,6 +3,6 @@
 cat $1 \
     | sed 's/\@\@ //g' \
     | ../tools/moses-scripts/scripts/recaser/detruecase.perl 2> /dev/null \
-    | ../tools/moses-scripts/scripts/tokenizer/detokenizer.perl -l ro 2>/dev/null \
+    | ../tools/moses-scripts/scripts/tokenizer/detokenizer.perl -l en 2>/dev/null \
     | ../tools/moses-scripts/scripts/generic/multi-bleu-detok.perl data/newsdev2016.en \
     | sed -r 's/BLEU = ([0-9.]+),.*/\1/'
